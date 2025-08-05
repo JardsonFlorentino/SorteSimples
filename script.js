c32e071007890dedd0416b03729a16d0a0c410b3
JardsonFlorentino <jardsonflorentino@gmail.com>
2025-08-05 00:39:49 -0300

    Melhorias-resultado

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
- function generateNumber() {
-    const min = Math.ceil(document.querySelector('.input-min').value)
-    const max = Math.floor(document.querySelector('.input-max').value)
-   
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-  
-   const resultContainer = document.getElementById('resultContainer');
+const modal = document.getElementById('resultModal');
+
+function generateNumber() {
+
+  const min = Math.ceil(document.querySelector('.input-min').value);
+  const max = Math.floor(document.querySelector('.input-max').value);
+
+
+  if (isNaN(min) || isNaN(max) || document.querySelector('.input-min').value.trim() === '' || document.querySelector('.input-max').value.trim() === '') {
+    alert("Por favor, preencha os dois campos com números.");
+    return;
+  }
+
+
+  if (min >= max) {
+    alert("O primeiro número deve ser MENOR que o segundo.");
+    return;
+  }
+
+  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
   const resultNumber = document.getElementById('resultNumber');
 
   resultNumber.textContent = result;
-  resultContainer.style.display = 'flex';
+  modal.style.display = 'flex';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    closeModal();
   }
-  
+}
